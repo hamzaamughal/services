29a36eb26fce5c091a51f6c547a620c407324819
abdulahad <abdulahad9691@gmail.com>
2024-12-07 13:47:11 +0500

    add animations

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,4 +1,37 @@
+// import React from "react";
+
+// export const Services = (props) => {
+//   return (
+//     <div id="services" className="text-center">
+//       <div className="container">
+//         <div className="section-title">
+//           <h2>Our Services</h2>
+//           <p>
+//             Building Businesses and Bridging Borders – Your Partner in Formation
+//             and Visa Success
+//           </p>
+//         </div>
+//         <div className="row">
+//           {props.data
+//             ? props.data.map((d, i) => (
+//                 <div key={`${d.name}-${i}`} className="col-md-4">
+//                   {" "}
+//                   <i className={d.icon}></i>
+//                   <div className="service-desc">
+//                     <h3>{d.name}</h3>
+//                     <p>{d.text}</p>
+//                   </div>
+//                 </div>
+//               ))
+//             : "loading"}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 import React from "react";
+// import "../App.css";  // Ensure to import the CSS file for styles
 
 export const Services = (props) => {
   return (
@@ -14,8 +47,10 @@ export const Services = (props) => {
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
+                <div
+                  key={`${d.name}-${i}`}
+                  className="col-md-4 service-item" // Apply the service-item class here
+                >
                   <i className={d.icon}></i>
                   <div className="service-desc">
                     <h3>{d.name}</h3>
